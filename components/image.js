068c306acd8b0f85c7908fd3bf3d5cf5c73df656
charlesgobina <charles.gobina@gmail.com>
2022-06-04 20:05:05 +0100

    Add blog posts

---
+++ components/image.js
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Charles Gobina"
+  />
+);
